7ec8f5ad7c6cd43b99ea54fa97f541e5555fd462
LESVAN555 <rastusaaluna@gmail.com>
2026-08-16 13:46:11 +0300

    Update HihW2Screen.java

--- src/main/java/com/hihw2/client/HihW2Screen.java
+++ src/main/java/com/hihw2/client/HihW2Screen.java
@@ -128,11 +128,18 @@ public class HihW2Screen extends Screen {
                 Text.literal("Fabric client mod"),
                 button -> {}
         ).dimensions(centerX - 100, 85, 200, 20).build());
-    }
-    
-@Override
+private static final net.minecraft.util.Identifier CAT_TEXTURE =
+            net.minecraft.util.Identifier.of("hihw2", "cat_bg.png");
+
+    @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context, mouseX, mouseY, delta);
+
+        int imgSize = 200;
+        int x = this.width / 2 - imgSize / 2;
+        int y = this.height / 2 - imgSize / 2;
+        context.drawTexture(CAT_TEXTURE, x, y, 0, 0, imgSize, imgSize, imgSize, imgSize);
+
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 8, 0xFFFFFF);
     }
